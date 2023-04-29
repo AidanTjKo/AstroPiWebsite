91f356a60113d75752dbec7a0aa72e418b65121f
Aidan Ko <aidan.tj.ko@gmail.com>
2023-04-29 16:07:10 -0400

    add monkey forest and monkey

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -265,8 +265,8 @@ export default function Home({ articles }) {
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Aidan Ko, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
+            City. I’m the founder and CEO of Monkey Forest, where we develop
+            technologies that empower regular monkeys to explore space on their
             own terms.
           </p>
           <div className="mt-6 flex gap-6">

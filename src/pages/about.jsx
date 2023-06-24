202078de4655b6c0f923c3672e6762972cbbabfd
Aidan Ko <aidan.tj.ko@gmail.com>
2023-06-24 01:22:15 -0400

    change apostrophes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -73,19 +73,19 @@ export default function About() {
               <p>
                 The only thing I loved more than computers as a kid was space.
                 When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
+                yard while wearing my older sister&apos;s motorcycle helmet, counted
                 down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                that with just a bit of momentum I&apos;d be able to get to orbit.
               </p>
               <p>
                 I spent the next few summers indoors working on a rocket design,
                 while I recovered from the multiple surgeries it took to fix my
                 badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
+                sent my dad&apos;s Blackberry into orbit and was able to transmit a
                 photo back down to our family computer from space.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
+                Today, I&apos;m the founder of Planetaria, where we&apos;re working on
                 civilian space suits and manned shuttle kits you can assemble at
                 home so that the next generation of kids really <em>can</em>{' '}
                 make it to orbit — from the comfort of their own backyards.

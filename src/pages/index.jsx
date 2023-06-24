738673a787dcd957b022dac577c16297668fd297
Aidan Ko <aidan.tj.ko@gmail.com>
2023-06-24 13:58:24 -0400

    more linting

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ export default function Index() {
             <div style={{backgroundImage:`url(${heroBanner.src})`,backgroundSize:"cover", height:"85vh",width:"100wh"}} className="bg-yellow-900 relative">
                 <p className="text-5xl font-semibold tracking-wider py-56 px-48 leading-10 text-center text-slate-300">Los Pollos El En Espacio</p>
                 <div className="absolute w-10 bottom-4 left-1/2">
-                    <svg className="scale-100 animate-pulse"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-6 h-6">
+                    <svg className="scale-100 animate-pulse w-6 h-6"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
                 </div>
